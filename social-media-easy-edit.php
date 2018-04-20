a3d2802a806bd69a2db7ce9efabe84b835818b15
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-20 13:04:23 +0100

    clean up plugin set up

--- social-media-easy-edit.php
+++ social-media-easy-edit.php
@@ -1,10 +1,8 @@
 <?php
-
 /*
 * Plugin Name: Social Media Easy Edit
 * Description: Plugin for editing social media
 * Version 1.0
 * Author: Iraida Artiles Corvo
 * License: GPL2
-*git config credential.username
-*/
+**/
