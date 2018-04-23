87ac4c8c5c0a02fddc14e15e03ad2ecfffcc3815
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-24 00:28:06 +0100

    input_password

---
+++ partials/inputs/password.php
@@ -0,0 +1,18 @@
+<?php 
+
+class input_password extends input{
+    function __construct($name = 'name', $id = '', $class = '', $value = ''){
+        parent::__construct($name, $id, $class, $value, 'password');
+    }
+
+    // function __set($name, $value){
+    //     switch( $name ):
+
+    //         default: 
+    //             parent::__set($name,$value);
+    //     endswitch;
+    // }
+
+}
+
+
