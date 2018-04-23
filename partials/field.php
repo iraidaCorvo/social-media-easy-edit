4fb0b35c4165b9484f23a14f7d6e6a87613cf673
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-24 00:17:46 +0100

    setter and serialize method

--- partials/field.php
+++ partials/field.php
@@ -18,5 +18,29 @@ class field{
 
         endswitch;
     }
+
+    function __set($name, $value){
+        switch( $name ):
+            case 'Required':
+                if( true === $value ) $this->props['required'] = $value;
+                else unset( $this->props['required']);
+                break;
+            case 'Value':
+                $this->value = $this->sanitize($value);
+                break;
+
+        endswitch;
+    }
+    function serialize_attrs(){
+        $attrs = '';
+        foreach(array_keys( $this->props ) as $attr):
+            if( !empty($this->props[$attr])):
+                $attrs .= "$attr='" .(($this->props[$attr] === true ) ? null : $this->props[$attr]) ."' ";
+            endif;
+        endforeach;
+        return $attrs;
+    }
+
+    function sanitize($value){return $value;}
 }
 ?>
