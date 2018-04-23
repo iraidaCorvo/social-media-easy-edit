875d85dafc8c959d289e05b3fe3b6c029ff2dc2b
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-24 00:27:16 +0100

    exception baseInput

--- partials/inputs/baseInput.php
+++ partials/inputs/baseInput.php
@@ -32,7 +32,24 @@ class input extends field{
         $attrs = $this->serialize_attrs();
         return "<input $attrs>";
     }
-
-
 }
+    class min_length extends Exception
+    {
+        // Redefinir la excepción, por lo que el mensaje no es opcional
+        public function __construct($message, $code = 0, Exception $previous = null) {
+            // algo de código
+        
+            // asegúrese de que todo está asignado apropiadamente
+            parent::__construct($message, $code, $previous);
+        }
+    
+        // representación de cadena personalizada del objeto
+        public function __toString() {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+    
+        public function funciónPersonalizada() {
+            echo "Una función personalizada para este tipo de excepción\n";
+        }
+    }
 
