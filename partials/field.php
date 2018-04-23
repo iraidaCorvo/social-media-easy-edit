49f6e40a08e9c8f7f830d234838b1f5eb34bb757
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-24 00:11:14 +0100

    constructor and setter

--- partials/field.php
+++ partials/field.php
@@ -1,5 +1,22 @@
 <?php
 class field{
+    protected $props = [
+        'value'    => '',
+        'class'    => '',
+        'name'     => '',
+        'id'       => ''
+    ];
     
+    function __construct($name = 'name', $id = '', $class = '', $value = ''){
+      
+    }
+
+    function __get($name){
+        switch( $name ):
+            case 'ID':
+                return $this->props['id'];
+
+        endswitch;
+    }
 }
 ?>
