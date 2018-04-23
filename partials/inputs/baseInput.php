0bbb97511f2c308dd455a38ddefae0924686db7b
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-24 00:24:07 +0100

    setter baseInput

--- partials/inputs/baseInput.php
+++ partials/inputs/baseInput.php
@@ -1,11 +1,24 @@
 <?php 
 
 class input extends field{
+    const INFINITE_SIZE = -1;
     protected $type;
+    private $minLength = self::INFINITE_SIZE;
     function __construct($name = 'name', $id = '', $class = '', $value = '', $type = 'text'){
         parent::__construct($name, $id, $class, $value);
         
         $this->props['type'] = $type;
     }
+    function __set($name, $value){
+        switch( $name ):
+            case 'MinLength':
+                if(!is_int($value)) throw new InvalidArgumentException('Not an integer.');
+                $this->minLength = (int)$value;
+            break;
+            default: 
+                parent::__set($name,$value);
+        endswitch;
+    }
+
 }
 
