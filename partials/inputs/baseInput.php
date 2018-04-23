ff1f54016fadc29f5ccfb7325ab06b5a476678e3
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-24 00:22:42 +0100

    constructor baseInput

--- partials/inputs/baseInput.php
+++ partials/inputs/baseInput.php
@@ -1,6 +1,11 @@
 <?php 
 
 class input extends field{
-    
+    protected $type;
+    function __construct($name = 'name', $id = '', $class = '', $value = '', $type = 'text'){
+        parent::__construct($name, $id, $class, $value);
+        
+        $this->props['type'] = $type;
+    }
 }
 
