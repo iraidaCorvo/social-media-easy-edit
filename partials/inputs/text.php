ed78a7c79ffa7da7020f0d363e9fef334ffc63a6
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-24 00:27:44 +0100

    input_text

---
+++ partials/inputs/text.php
@@ -0,0 +1,18 @@
+<?php 
+
+class input_text extends input{
+    
+    function __construct($name = 'name', $id = '', $class = '', $value = ''){
+        parent::__construct($name, $id, $class, $value);
+    }
+ 
+    function __set($name, $value){
+        switch( $name ):
+
+        default: 
+            parent::__set($name,$value);
+        endswitch;
+    }
+
+
+}
