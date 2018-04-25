ac7073b33178543b3f5ff6f9407621cc0135e171
Iraida Corvo <iraida.corvo@gmail.com>
2018-04-25 22:18:50 +0100

    Add plugin to menu page

--- social-media-easy-edit.php
+++ social-media-easy-edit.php
@@ -14,4 +14,23 @@ function testing(){
    // exit;
     //echo $miInput;
 }
+add_action('admin_menu','socialMedia_menu');
+function socialMedia_menu(){
+    //$sesuSettingsPage = new SectionSettings( );
+    add_menu_page(
+        'Social Media Easy Edit',
+        'SM Easy Edit',
+        'manage_options',
+        'Social Media',
+        'social_media_options_twitter',
+        'dashicons-groups',
+        '40'
+    );
+
+    add_submenu_page( 'Social Media', 'Twitter', 'Twitter', 'manage_options', 'Social Media');
+    add_submenu_page( 'Social Media', 'Facebook', 'Facebook', 'manage_options', 'SM_options_facebook', 'SM_options_facebook');
+    add_submenu_page( 'Social Media', 'Linkedin', 'Linkedin', 'manage_options', 'SM_options_submenu1');
+    add_submenu_page( 'Social Media', 'Spotify', 'Spotify', 'manage_options', 'SM_options_submenu2');
+}
+
 
