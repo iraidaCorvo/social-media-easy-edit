94fe4c9b59247eef3c4e345cce53dcb0f58249fb
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-03 20:30:05 +0100

    Add Path

--- partials/inputs/text.php
+++ partials/inputs/text.php
@@ -1,5 +1,5 @@
 <?php 
-
+include_once SMEE_PARTIALS_PATH . 'inputs/baseInput.php';
 class input_text extends input{
     
     function __construct($name = 'name', $id = '', $class = '', $value = ''){
