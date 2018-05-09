46e411ea635834496e5bb3a18021ab25eb6fc8d9
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-09 20:54:28 +0100

    Set Value. Add label.

--- partials/inputs/password.php
+++ partials/inputs/password.php
@@ -1,12 +1,16 @@
 <?php 
 
 class input_password extends input{
-    function __construct($name = 'name', $id = '', $class = '', $value = ''){
-        parent::__construct($name, $id, $class, $value, 'password');
+    function __construct($name = 'name', $id = '', $class = '', $value = '', $label = ''){
+        
+        parent::__construct($name, $id, $class, $value, 'password', $label);
     }
 
     function __set($name, $value){
         switch( $name ):
+            case 'Value':
+                parent::__set($name,'');
+            break;
             default: 
                parent::__set($name,$value);
          endswitch;
