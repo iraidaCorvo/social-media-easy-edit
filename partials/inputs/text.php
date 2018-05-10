11bf2b5e361159e65faa4f49d73953cbf157448d
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-10 23:02:00 +0100

    Update text constructor

--- partials/inputs/text.php
+++ partials/inputs/text.php
@@ -2,15 +2,13 @@
 include_once SMEE_PARTIALS_PATH . 'inputs/baseInput.php';
 class input_text extends input{
     
-    function __construct($name = 'name', $id = '', $class = '', $value = '', $label = ''){
-        
-        parent::__construct($name, $id, $class, $value, 'text', $label);
+    function __construct($name, $args=[]){
+        parent::__construct($name, array_merge($args, ['type' => 'text']));
         
     }
  
     function __set($name, $value){
         switch( $name ):
-
         default: 
             parent::__set($name,$value);
         endswitch;
