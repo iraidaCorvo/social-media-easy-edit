bb0c6471b52c5e2fd60536c358848d904b201b6d
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-11 14:46:12 +0100

    Improve contruct. Add type.

--- partials/inputs/text.php
+++ partials/inputs/text.php
@@ -3,8 +3,8 @@ include_once SMEE_PARTIALS_PATH . 'inputs/baseInput.php';
 class input_text extends input{
     
     function __construct($name, $args=[]){
-        parent::__construct($name, array_merge($args, ['type' => 'text']));
-        
+        $this->props['type']='text';
+        parent::__construct($name, $args); 
     }
  
     function __set($name, $value){
