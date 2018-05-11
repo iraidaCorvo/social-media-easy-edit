924c853ec124d12aa783d5d9bc28861d6812ffe5
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-11 14:45:48 +0100

    Improve contruct. Add type.

--- partials/inputs/submit.php
+++ partials/inputs/submit.php
@@ -2,8 +2,9 @@
 include_once SMEE_PARTIALS_PATH . 'inputs/baseInput.php';
 class input_submit extends input{
     
-    function __construct($name, $args=[]){
-        parent::__construct($name, array_merge($args, ['type' => 'submit']));
+    function __construct($args=[]){
+        $this->props['type']='submit';
+        parent::__construct('', $args);
         
     }
  
