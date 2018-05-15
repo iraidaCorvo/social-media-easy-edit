0c9351fd48ae26d681e2824b77d3c5ec5ecaab75
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-15 19:51:34 +0100

    Array values select. Create set. Delete not used classes.

--- partials/select/select.php
+++ partials/select/select.php
@@ -1,6 +1,12 @@
 <?php 
 require_once "field.php";
 class select extends field{
+    protected $values = [
+        'value1'    => '',
+        'value2'    => '',
+        'value3'     => '',
+        'value4'       => ''
+    ];
 
     //array con opciones
     function __construct($name = 'name', $args=[]){
@@ -8,29 +14,10 @@ class select extends field{
         parent::__construct($name, $args);
     }
 
-    function render(){}
-    function sanitize(){}
-
-    function __toString(){
-        $id    = $this->id ;
-        $name  = $this->name;
-        $class = $this->class;
-        $type  = $this->type;
-        $value = $this->value;
-
-        $text = parent::__toString();        
-        $text .= '
-            <form action="#" method="POST">   
-                <select id="'.$id.'" class="'.$class.'" name="'.$name.'" value="'.$value.'">
-                    <option value="rojo">Rojo</option>
-                    <option value="verde">Verde</option>
-                    <option value="azul">Azul</option>
-                    <option value="negro">Negro</option>
-                </select>
-            </form>
-        ';
-        $select = $_POST[$name];  
-        return $text;
+    function __set($name, $value){
+        switch( $name ):
+        default: 
+            parent::__set($name,$value);
+        endswitch;
     }
-
 }
