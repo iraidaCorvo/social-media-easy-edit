44532be2ec370dd107607c848c92bab6fa7759f0
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-15 22:03:09 +0100

    Create view for select. Add options.

--- views/select.php
+++ views/select.php
@@ -1,7 +1,7 @@
 <label><?php echo $label ?></label>
-<select>
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
+<select name="<?php echo $attrsValues['name'] ?>">
+    <option value='<?php echo $attrsValues['value1'] ?>'> <?php echo $attrsValues['value1'] ?></option>
+    <option value='<?php echo $attrsValues['value2'] ?>'><?php echo $attrsValues['value2'] ?></option>
+    <option value='<?php echo $attrsValues['value3'] ?>'><?php echo $attrsValues['value3'] ?></option>
+    <option value='<?php echo $attrsValues['value4'] ?>'><?php echo $attrsValues['value4'] ?></option>
  </select>
