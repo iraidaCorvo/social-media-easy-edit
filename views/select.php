1b6e961ed55db9dc304c05b112500f7da85adacf
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-15 19:33:54 +0100

    Add view for select

---
+++ views/select.php
@@ -0,0 +1,7 @@
+<label><?php echo $label ?></label>
+<select>
+    <option value=""></option>
+    <option value=""></option>
+    <option value=""></option>
+    <option value=""></option>
+ </select>
