da121a8aaf8e78cf6e144d167c645f9c47a396db
Iraida Corvo <iraida.corvo@gmail.com>
2018-05-16 21:41:26 +0100

    Improve select view

--- views/select.php
+++ views/select.php
@@ -1,7 +1,7 @@
 <label><?php echo $label ?></label>
-<select name="<?php echo $attrsValues['name'] ?>">
-    <option value='<?php echo $attrsValues['value1'] ?>'> <?php echo $attrsValues['value1'] ?></option>
-    <option value='<?php echo $attrsValues['value2'] ?>'><?php echo $attrsValues['value2'] ?></option>
-    <option value='<?php echo $attrsValues['value3'] ?>'><?php echo $attrsValues['value3'] ?></option>
-    <option value='<?php echo $attrsValues['value4'] ?>'><?php echo $attrsValues['value4'] ?></option>
+<select <?php echo $attrs ?>>
+    <?php foreach($options as $value => $text):?>
+        <option <?php echo ($value== $selected) ? 'selected': null ?> value='<?php echo $value?>'><?php echo $text ?></option>
+    <?php endforeach ?>
  </select>
+ 
