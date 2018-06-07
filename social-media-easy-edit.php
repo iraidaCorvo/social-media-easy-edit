1eff4ae5990eed3afa2ee34446c1a7e1aec7344e
iraidaCorvo <iraida.arcorvo@gmail.com>
2018-06-07 22:11:19 +0100

    Update creator with the builder.

--- social-media-easy-edit.php
+++ social-media-easy-edit.php
@@ -13,140 +13,17 @@ define( 'SMEE_VIEWS', SMEE_PATH . 'views/');
 define( 'SMEE_SELECT_PATH', SMEE_PARTIALS_PATH . 'select/');
 define( 'SMEE_PLUGIN_SHORT_NAME' , 'smee');
 define( 'SMEE_PLUGIN_DOMAIN'     , 'smee');
+
+
 include_once 'section.php';
 include_once 'configurationPage.php';
 include_once 'smee_menu.php';
+include_once 'admin-page-setup.php';
 
-    $smee_menu = new smee_menu_page();
-    $smee_menu
-    ->add_page(
-        'Social Media Easy Edit',
-        'SM Easy Edit',
-        'manage_options',
-        'social_media'
-    )
-    ->add_section([
-        'id'    => 'config_section',
-        'title' => 'Configuration Page'
-    ])
-    
-    ->add_field([
-        'type' => 'text',
-        'name' => 'testing',
-        'label' => 'Configuration',
-        'placeholder' => 'Text'
-    ])
-    ->add_page(
-        'Facebook',
-        'Facebook',
-        'manage_options',
-        'social_media3'
-    )
-    ->add_section([
-        'id'    => 'facebook_section',
-        'title' => 'Configuration Page Facebook'
-    ])
-    ->add_field([
-        'type' => 'text',
-        'name' => 'fb-app-id',
-        'label' =>'App ID',
-        'placeholder' => 'app id'
-    ])
-    ->add_field([
-        'type' => 'password',
-        'name' => 'fb-secret',
-        'label' => 'Secret Facebook',
-        'placeholder' => 'secret facebook'
-    ])
-    ->add_field([
-        'type' => 'text',
-        'name' => 'page-id',
-        'label' =>'Page ID',
-        'placeholder' => 'page id'
-    ])
-    ->add_page(
-        'Instagram',
-        'Instagram',
-        'manage_options',
-        'social_media6'
-    )
-    ->add_section([
-        'id'    => 'instagram_section',
-        'title' => 'Configuration Page Instagram'
-    ])
-    ->add_field([
-        'type' => 'text',
-        'name' => 'inst-client-id',
-        'label' =>'Client Instagram ID',
-        'placeholder' => 'client id'
-    ])
-    ->add_field([
-        'type' => 'password',
-        'name' => 'inst-client-secret',
-        'label' => 'Client Instagram secret',
-        'placeholder' => 'client secret'
-    ])
-    ->add_field([
-        'type' => 'text',
-        'name' => 'inst-access-token',
-        'label' =>'Access token',
-        'placeholder' => 'access token'
-    ])
-    ->add_field([
-        'type' => 'text',
-        'name' => 'inst-callback',
-        'label' =>'Callback URL',
-        'placeholder' => 'callback url'
-    ])
 
-    ->add_page(
-        'Linkedin',
-        'Linkedin',
-        'manage_options',
-        'social_media4'
-    )
-    ->add_page(
-        'Spotify',
-        'Spotify',
-        'manage_options',
-        'social_media5'
-    )
-    ->add_page(
-        'Twitter',
-        'Twitter',
-        'manage_options',
-        'social_media2'
-    )
-    ->add_section([
-        'id'    => 'twitter_section',
-        'title' => 'Configuration Page Twitter'
-    ])
-    ->add_field([
-        'type' => 'text',
-        'name' => 'tw-key',
-        'label' =>'Consumer Twitter Key',
-        'placeholder' => 'twitter key'
-    ])
-    ->add_field([
-        'type' => 'password',
-        'name' => 'tw-secret',
-        'label' =>'Consumer Twitter Secret',
-        'placeholder' => 'twitter secret'
-    ])
-    ->add_field([
-        'type' => 'text',
-        'name' => 'tw-token',
-        'label' =>'Oauth Twitter Token',
-        'placeholder' => 'twitter oauth token'
-    ])
-    ->add_field([
-        'type' => 'password',
-        'name' => 'tw-token-secret',
-        'label' =>'Oauth Twitter Token secret',
-        'placeholder' => 'twitter oauth secret'
-    ])
+$smee_menu = new smee_menu_page();
 
-    ;
+    $smee_menu->borjis_builder($config_page_data);
     function generate_form($form, $with_submit=true){
          
         $miForm = new section($with_submit);
