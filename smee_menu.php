8e331700725706c2d149598b032ce1f0f95a6c7a
iraidaCorvo <iraida.arcorvo@gmail.com>
2018-06-07 22:10:58 +0100

    Edit add_page to use it with differente parameters.

--- smee_menu.php
+++ smee_menu.php
@@ -4,7 +4,25 @@ class smee_menu_page{
     private $pages = [];
     private $current_page_slug = null; //Apunta a la página usada recientemente
 
-    public function add_page(string $page_title, string $menu_title, string $capability, string $menu_slug){
+    public function add_page(  ){
+        $args = func_get_args();
+        $func_num_args = func_num_args();
+        if( $func_num_args === 1 ):
+            if(is_array($args[0])):
+                extract($args[0]);
+            else:
+                die('not valid');
+            endif;
+            //showArrayAndDie($args[0]);
+        elseif( $func_num_args === 4):
+            $page_title = $args[0];
+            $menu_title = $args[1];
+            $capability = $args[2];
+            $menu_slug  = $args[3];
+        else: 
+            die('not valid');            
+        endif;
+
         $this->current_page_slug = $menu_slug;
         $this->pages[$menu_slug] = new configurationPage (
             $page_title, $menu_title, $capability, $menu_slug, $this->parent_slug
@@ -13,6 +31,7 @@ class smee_menu_page{
         if(empty($this->parent_slug)) $this->parent_slug = $this->current_page_slug;
         return $this;
     }
+
     public function add_section( array $args ){
         if(!empty($this->current_page_slug)): 
             $current_page_slug = $this->current_page_slug;
@@ -26,5 +45,19 @@ class smee_menu_page{
             $this->pages[$current_page_slug]->add_field($args);
         endif;
         return $this;
+    }
+
+    public function borjis_builder(array $configuration_pages){
+        foreach( $configuration_pages as $page_slug => $configuration_page ):
+            $this->add_page($configuration_page['page_attrs']);
+            foreach($configuration_page['sections'] as $section_slug => $section ):
+                $this->add_section($section['section_attrs']);
+                foreach( $section['fields'] as $field_slug => $field ):
+                    $this->add_field($field);
+                endforeach;
+            endforeach;
+        endforeach;
+        return $this;
+
     }
 }
