61c93228f364fc2b206d6c8ae83dea2b247d92d0
iraidaCorvo <iraida.arcorvo@gmail.com>
2018-06-08 21:08:09 +0100

    Update select with new attributes

--- views/select.php
+++ views/select.php
@@ -1,5 +1,5 @@
-<label><?php echo $label ?></label>
-<select <?php echo $attrs ?>>
+<?php showText($selected)?>
+<select <?php echo $serialize_attrs ?>>
     <?php foreach($options as $value => $text):?>
         <option <?php echo ($value== $selected) ? 'selected': null ?> value='<?php echo $value?>'><?php echo $text ?></option>
     <?php endforeach ?>
